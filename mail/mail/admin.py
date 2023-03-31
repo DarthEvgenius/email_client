25cc7b6e6e32d639127184afe7810dbc2da65305
DarthEvgenius <clay14@yandex.ru>
2023-03-31 12:53:49 +0300

    Compose email done

--- mail/mail/admin.py
+++ mail/mail/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+# admin@admin.com => admin0000
+# harry@harry.com => harry0000
+# ron@ron.com => ron0000
 # Register your models here.
--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 
   // Use buttons to toggle between views
   document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
@@ -6,8 +6,12 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
 
+  document.querySelector('#compose-form').onsubmit = process_form;
+
+
   // By default, load the inbox
   load_mailbox('inbox');
+
 });
 
 function compose_email() {
@@ -23,11 +27,44 @@ function compose_email() {
 }
 
 function load_mailbox(mailbox) {
-  
+
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+}
+
+function process_form() {  
+  // Get inputs form the form
+  const recipients = document.querySelector('#compose-recipients').value;
+  const subject = document.querySelector('#compose-subject').value;
+  const body = document.querySelector('#compose-body').value;
+
+  // Make a POST request to the server
+  fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+      recipients: recipients,
+      subject: subject,
+      body: body
+    })
+  })
+  .then(response => response.json())
+  // Check the response
+  .then(result => {
+    console.log(result);
+    // If there is an error response, show the error message in the input's placeholder
+    if (result.error) {
+      let error_field = document.querySelector('#compose-recipients');
+      error_field.value = '';
+      error_field.placeholder = result.error;
+    } else {
+      load_mailbox('sent');
+    }
+  });
+
+  return false;
 }
