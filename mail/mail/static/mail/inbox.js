b8f51060e9a7ae6078fdad1ca6499779e446a921
DarthEvgenius <clay14@yandex.ru>
2023-04-01 18:27:11 +0300

    View Email done

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -35,6 +35,45 @@ function load_mailbox(mailbox) {
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 
+  // Request the server for the mailbox
+  fetch(`/emails/${mailbox}`)
+  .then(response => response.json())
+  .then(emails => {
+    // Print emails
+    console.log(emails);
+
+    // Each email shouldbe rendered in its own box (e.g. as a <div> with a border) that displays who the email is from, what the subject line is, and the timestamp of the email.
+    emails.forEach(email => {
+      const mail_row = document.createElement('div');
+      mail_row.className = 'row border rounded my-2';
+      // If the email is unread => white background. If read => gray background
+      if (email.read === true) {
+        mail_row.classList.add('bg-secondary-subtle');
+      } else {
+        mail_row.classList.add('bg-light');
+      }
+
+      const mail_sender = document.createElement('div');
+      mail_sender.className = 'col-4 p-2';
+      mail_sender.innerHTML = `From: ${email.sender}`;
+
+      const mail_subject = document.createElement('div');
+      mail_subject.className = 'col-4 p-2 border-left border-right';
+      mail_subject.innerHTML = `Subject: ${email.subject}`;
+
+      const mail_time = document.createElement('div');
+      mail_time.className = 'col-4  p-2';
+      mail_time.innerHTML = `${email.timestamp}`;
+
+      mail_row.appendChild(mail_sender);
+      mail_row.appendChild(mail_subject);
+      mail_row.appendChild(mail_time);
+
+      document.querySelector('#emails-view').append(mail_row);
+    });
+
+    
+});
 }
 
 function process_form() {  
